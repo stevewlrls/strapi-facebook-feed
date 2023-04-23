8c8bcd52c713b1c6c0ada0ca6ac4e1eb7d6c08ee
Steven Wheeler <stevewlrls@gmail.com>
2023-04-23 07:51:04 +0100

    Add lifecycle hook on delete to remove pictures.

--- server/content-types/facebook-post/index.js
+++ server/content-types/facebook-post/index.js
@@ -1,7 +1,24 @@
 'use strict';
 
+const fs = require('node:fs/promises');
+
 const schema = require('./schema');
 
 module.exports = {
   schema,
+  lifecycles: {
+    async beforeDelete(event) {
+      const item = await strapi.db.query(
+        'plugin::facebook-feed.facebook-post'
+      ).findOne({
+        select: ['postID'],
+        where: event.params.where
+      });
+      if (item) {
+        const picture = item.postID + '.webp';
+        await fs.rm('./public/facebook-feed/' + picture)
+          .catch(() => {}); // Ignore errors
+      }
+    }
+  }
 }
